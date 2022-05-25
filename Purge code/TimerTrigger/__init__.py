5c0212d4e580e014a1943ec4c25451be1d21119b
dsashishgarg <ashishgarg@live.in>
2022-05-25 10:58:25 +0530

    Updated Code

--- Purge code/TimerTrigger/__init__.py
+++ Purge code/TimerTrigger/__init__.py
@@ -3,13 +3,15 @@ import azure.functions as func
 import azure.durable_functions as df
 from azure.durable_functions.models.DurableOrchestrationStatus import OrchestrationRuntimeStatus
 from datetime import datetime, timedelta
-import os
 
-
-async def main(mytimer: func.Timer Request, starter: str):
+async def main(mytimer: func.TimerRequest, starter: str):
+    logging.warn("working")
     client = df.DurableOrchestrationClient(starter)
     created_time_from = datetime.min
+    logging.warning(created_time_from)
     created_time_to = datetime.today()
-    runtime_status = [OrchestrationRuntimeStatus.completed]
-    response = await client.purge_instance_history_by(created_time_from, created_time_to, runtime_status)
-    logging.info(response)
+    logging.warning(created_time_to)
+    runtime_statuses = [OrchestrationRuntimeStatus.Completed]
+
+    purgeHistoryResult = client.purge_instance_history_by(created_time_from, created_time_to, runtime_statuses)
+    logging.warn(purgeHistoryResult.close)
