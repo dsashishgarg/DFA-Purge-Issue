bf21a4f923ec341b9bf848f4c08121e5716ffb0c
dsashishgarg <ashishgarg@live.in>
2022-05-27 14:32:14 +0530

    Added Await Operation

--- Purge code/TimerTrigger/__init__.py
+++ Purge code/TimerTrigger/__init__.py
@@ -5,9 +5,14 @@ from azure.durable_functions.models.DurableOrchestrationStatus import Orchestrat
 from datetime import datetime, timedelta
 
 async def main(mytimer: func.TimerRequest, starter: str):
+    logging.warning("Time Trigger App received a request")
     client = df.DurableOrchestrationClient(starter)
     created_time_from = datetime.min
     created_time_to = datetime.today()
     runtime_statuses = [OrchestrationRuntimeStatus.Completed]
-    purgeHistoryResult = client.purge_instance_history_by(created_time_from, created_time_to, runtime_statuses)
-    logging.info(purgeHistoryResult)
+    logging.warning("created_time_from: " + str(created_time_from))
+    logging.warning("created_time_to: " + str(datetime.today()))
+    logging.warning("runtime_statuses: " + str(runtime_statuses))
+
+    purgeHistoryResult = await client.purge_instance_history_by(created_time_from, created_time_to, runtime_statuses)
+    logging.warning(purgeHistoryResult)
